ff7829f72e02596f4893e6eeb67dae75452fe65b
jesusjaferlo <jesusjaferlo@gmail.com>
2016-12-16 21:14:19 +0100

    Created Booking Service Controller

---
+++ SpringRestFulWS/src/com/ferlodevs/service/BookingServiceController.java
@@ -0,0 +1,14 @@
+package com.ferlodevs.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service")
+
+public class BookingServiceController {
+	// Logger instance
+		private static final Logger logger = Logger.getLogger(SimpleRestController.class);
+
+}
