741ea908494809fd3887f10079ec59947686d9a3
jesusjaferlo <jesusjaferlo@gmail.com>
2016-12-17 00:38:06 +0100

    change uri booking service

--- SpringRestFulWS/src/com/ferlodevs/service/BookingServiceController.java
+++ SpringRestFulWS/src/com/ferlodevs/service/BookingServiceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/service")
+@RequestMapping("/service/payment")
 
 public class BookingServiceController {
 	// Logger instance
