16f6130337ba2812996037ca9b22ac181787d6f5
jesusjaferlo <jesusjaferlo@gmail.com>
2016-12-17 01:23:45 +0100

    Create order service controller

---
+++ SpringRestFulWS/src/com/ferlodevs/service/OrderServiceController.java
@@ -0,0 +1,12 @@
+package com.ferlodevs.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/booking")
+public class OrderServiceController {
+	// Logger instance
+		private static final Logger logger = Logger.getLogger(SimpleRestController.class);
+}
